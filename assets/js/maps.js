95b929d9c00448364309be13fd43c84d9fe61e86
Jesson96 <ianjesson96@gmail.com>
2021-03-15 18:09:32 +0000

    added maps.js function to display map and added margin bottom on map id to give some space at the bottom of the site

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,3 +1,10 @@
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
-  
+        zoom: 12,
+        center: {
+            lat: 53.350140,
+            lng: -6.266155
+        }
+    });
+
+}
