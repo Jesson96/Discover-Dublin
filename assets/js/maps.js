e48793bbbc88b29708e3dbb4726dfba9e3ccf0a8
Jesson96 <ianjesson96@gmail.com>
2021-03-15 20:50:59 +0000

    made an attractions marker object to be called upon when implementing an onclick function for associated button

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,5 +1,7 @@
 function initMap() {
-    var map = new google.maps.Map(document.getElementById("map"), {
+    //new map
+    const map = new google.maps.Map(document.getElementById("map"), {
+    // map options
         zoom: 12,
         center: {
             lat: 53.350140,
@@ -7,4 +9,23 @@ function initMap() {
         }
     });
 
+    // Marker locations and affiliated content
+    var attractionsMarker = [
+        {
+            location: {lat: 53.3438,lng: 6.2546},
+            content: "<h4>Trinity College Dublin</h4><p>Visit this esteemed research university and the home of the Book of Kells, the sole constituent college of the University of Dublin.</p>"
+    },
+        {
+            location: {lat: 53.3728,lng: 6.2768},
+            content: "<h4>Glasnevin Cemetary</h4><p>t holds the graves and memorials of several notable figures who have helped shaped Irish history and culture.</p>"
+    },
+        {
+            location: {lat: 53.3429,lng: 6.2674},
+            content: "<h4>Dublin Castle</h4><p>Erected in the early thirteenth century on the site of a Viking settlement. It is now a major government complex and a key tourist attraction.</p>"
+    },
+        {
+            location: {lat: 53.3449,lng: 6.2675},
+            content: "<h4>Temple Bar</h4><p> It is promoted as Dublin's 'cultural quarter' and, as a centre of Dublin's city centre's nightlife, is a tourist destination.</p>"
+    },
+    ]
 }
