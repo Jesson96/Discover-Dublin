1ab647d9661425c5ec14a84234e2afd803294896
Jesson96 <ianjesson96@gmail.com>
2021-03-19 20:34:09 +0000

    added basic function for sending a message in the contact form in sendEmail.js

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,1 +1,14 @@
-console.log("Hello");
+function sendMail (contactForm) {
+    emailjs.send("gmail", "contact-form", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.messagequery.value
+    })
+    .then(
+        function(response) {
+            console.log("Message sent!", response);
+        },
+        function(error) {
+            console.log("Failed to send message!", error);
+        });
+}
