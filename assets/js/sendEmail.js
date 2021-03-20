18bf1f06ad0839e4ae4c626c50574d319e48a736
Jesson96 <ianjesson96@gmail.com>
2021-03-20 16:21:39 +0000

    updated sendEmail.js from console.log to alert the user if their form has been submitted successfully or not

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,15 +1,18 @@
-function sendMail (contactForm) {
+function sendMail(contactForm) {
     emailjs.send("gmail", "contact-form", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
         "message_query": contactForm.text.value
     })
-    .then(
-        function(response) {
-            console.log("Message sent!", response);
-        },
-        function(error) {
-            console.log("Failed to send message!", error);
-        });
+        .then( // https://stackoverflow.com/questions/58789965/confirmation-message-not-displaying-on-javascript-contact-form code to alert user if message has been sent successfully.
+            function (response) {
+                if (response.status == 200 && response.text == 'OK')
+                    alert('Your message was sent successfully!');
+                else
+                    alert('Oops, there was a problem sending your message. Please try again!');
+            }, function () {
+                alert('Sorry there was a problem. Please try again!');
+                document.getElementsByID('myForm').reset();
+            });
     return false;
 }
